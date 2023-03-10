4648e7d5b5a7c1c00a3ac11da7d66696f3eb83ec
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-03-10 22:15:29 +0530

    Completed About Section

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-content");
 }
+
+var navbarlinks = document.getElementsByClassName("navbar-links");
+var websitecontents = document.getElementsByClassName("website-contents");
+
+function opensection(sectionname) {
+  for (navbarlink of navbarlinks) {
+    navbarlink.classList.remove("active-link");
+  }
+
+  for(websitecontent of websitecontents) {
+    websitecontent.classList.remove("active-section");
+  }
+
+  document.getElementById(sectionname).classList.add("active-section");
+}
