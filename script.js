cbd88272c26863d8ec0ba5d1b5a636e16d4972a1
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-03-11 14:17:48 +0530

    Added CSS For Smaller Screens

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Description: This file contains the JavaScript code for the website.
+
+// This function is used to open the section of the website that is clicked on the navbar.
+var navbarlinks = document.getElementsByClassName("navbar-links");
+var websitecontents = document.getElementsByClassName("website-contents");
+
+function opensection(sectionname) {
+  for (navbarlink of navbarlinks) {
+    navbarlink.classList.remove("active-link");
+  }
+
+  for (websitecontent of websitecontents) {
+    websitecontent.classList.remove("active-section");
+  }
+
+  document.getElementById(sectionname).classList.add("active-section");
+}
+
+// This function is used to open the tab of the website that is clicked on the navbar.
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -13,17 +32,16 @@ function opentab(tabname) {
   document.getElementById(tabname).classList.add("active-content");
 }
 
-var navbarlinks = document.getElementsByClassName("navbar-links");
-var websitecontents = document.getElementsByClassName("website-contents");
+// This function is used to open the side menu of the website.
+var sidemenu = document.getElementById("side-menu");
+var sidemenubutton = document.getElementById("open-menu-icon");
 
-function opensection(sectionname) {
-  for (navbarlink of navbarlinks) {
-    navbarlink.classList.remove("active-link");
-  }
+function openmenu() {
+  sidemenu.style.right = "0%";
+  sidemenubutton.style.display = "none";
+}
 
-  for(websitecontent of websitecontents) {
-    websitecontent.classList.remove("active-section");
-  }
-
-  document.getElementById(sectionname).classList.add("active-section");
+function closemenu() {
+  sidemenu.style.right = "-50%";
+  sidemenubutton.style.display = "block";
 }
